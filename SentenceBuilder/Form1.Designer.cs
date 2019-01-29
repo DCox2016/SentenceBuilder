efd851e0daff8182ee944bdbf0680bddc9499a61
DCox2016 <leadsingerstarke@yahoo.com>
2019-01-29 09:32:03 -0500

    C# Sentence Builder

---
+++ SentenceBuilder/Form1.Designer.cs
@@ -0,0 +1,400 @@
+﻿namespace SentenceBuilder
+{
+    partial class SentenceBuilder
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.upcaseA = new System.Windows.Forms.Button();
+            this.lowercaseA = new System.Windows.Forms.Button();
+            this.buttonAn = new System.Windows.Forms.Button();
+            this.lowercaseAn = new System.Windows.Forms.Button();
+            this.uppercaseThe = new System.Windows.Forms.Button();
+            this.lowercaseThe = new System.Windows.Forms.Button();
+            this.buttonMan = new System.Windows.Forms.Button();
+            this.buttonWoman = new System.Windows.Forms.Button();
+            this.buttonDog = new System.Windows.Forms.Button();
+            this.buttonCat = new System.Windows.Forms.Button();
+            this.buttonCar = new System.Windows.Forms.Button();
+            this.buttonBicyle = new System.Windows.Forms.Button();
+            this.buttonBeautiful = new System.Windows.Forms.Button();
+            this.buttonBig = new System.Windows.Forms.Button();
+            this.buttonSmall = new System.Windows.Forms.Button();
+            this.buttonStrange = new System.Windows.Forms.Button();
+            this.buttonLookedAt = new System.Windows.Forms.Button();
+            this.buttonRode = new System.Windows.Forms.Button();
+            this.buttonSpokeTo = new System.Windows.Forms.Button();
+            this.buttonLaughedAt = new System.Windows.Forms.Button();
+            this.buttonDrove = new System.Windows.Forms.Button();
+            this.buttonSpace = new System.Windows.Forms.Button();
+            this.buttonPeriod = new System.Windows.Forms.Button();
+            this.buttonExclamation = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.sentenceDisplay = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // upcaseA
+            // 
+            this.upcaseA.Location = new System.Drawing.Point(200, 28);
+            this.upcaseA.Name = "upcaseA";
+            this.upcaseA.Size = new System.Drawing.Size(115, 73);
+            this.upcaseA.TabIndex = 0;
+            this.upcaseA.Text = "A";
+            this.upcaseA.UseVisualStyleBackColor = true;
+            this.upcaseA.Click += new System.EventHandler(this.upcaseA_Click);
+            // 
+            // lowercaseA
+            // 
+            this.lowercaseA.Location = new System.Drawing.Point(344, 28);
+            this.lowercaseA.Name = "lowercaseA";
+            this.lowercaseA.Size = new System.Drawing.Size(107, 73);
+            this.lowercaseA.TabIndex = 1;
+            this.lowercaseA.Text = "a";
+            this.lowercaseA.UseVisualStyleBackColor = true;
+            this.lowercaseA.Click += new System.EventHandler(this.lowercaseA_Click);
+            // 
+            // buttonAn
+            // 
+            this.buttonAn.Location = new System.Drawing.Point(473, 30);
+            this.buttonAn.Name = "buttonAn";
+            this.buttonAn.Size = new System.Drawing.Size(105, 71);
+            this.buttonAn.TabIndex = 2;
+            this.buttonAn.Text = "An";
+            this.buttonAn.UseVisualStyleBackColor = true;
+            this.buttonAn.Click += new System.EventHandler(this.buttonAn_Click);
+            // 
+            // lowercaseAn
+            // 
+            this.lowercaseAn.Location = new System.Drawing.Point(593, 28);
+            this.lowercaseAn.Name = "lowercaseAn";
+            this.lowercaseAn.Size = new System.Drawing.Size(129, 73);
+            this.lowercaseAn.TabIndex = 3;
+            this.lowercaseAn.Text = "an";
+            this.lowercaseAn.UseVisualStyleBackColor = true;
+            this.lowercaseAn.Click += new System.EventHandler(this.lowercaseAn_Click);
+            // 
+            // uppercaseThe
+            // 
+            this.uppercaseThe.Location = new System.Drawing.Point(740, 28);
+            this.uppercaseThe.Name = "uppercaseThe";
+            this.uppercaseThe.Size = new System.Drawing.Size(127, 73);
+            this.uppercaseThe.TabIndex = 4;
+            this.uppercaseThe.Text = "The";
+            this.uppercaseThe.UseVisualStyleBackColor = true;
+            this.uppercaseThe.Click += new System.EventHandler(this.uppercaseThe_Click);
+            // 
+            // lowercaseThe
+            // 
+            this.lowercaseThe.Location = new System.Drawing.Point(891, 30);
+            this.lowercaseThe.Name = "lowercaseThe";
+            this.lowercaseThe.Size = new System.Drawing.Size(115, 71);
+            this.lowercaseThe.TabIndex = 5;
+            this.lowercaseThe.Text = "the";
+            this.lowercaseThe.UseVisualStyleBackColor = true;
+            this.lowercaseThe.Click += new System.EventHandler(this.lowercaseThe_Click);
+            // 
+            // buttonMan
+            // 
+            this.buttonMan.Location = new System.Drawing.Point(150, 117);
+            this.buttonMan.Name = "buttonMan";
+            this.buttonMan.Size = new System.Drawing.Size(153, 73);
+            this.buttonMan.TabIndex = 6;
+            this.buttonMan.Text = "man";
+            this.buttonMan.UseVisualStyleBackColor = true;
+            this.buttonMan.Click += new System.EventHandler(this.buttonMan_Click);
+            // 
+            // buttonWoman
+            // 
+            this.buttonWoman.Location = new System.Drawing.Point(320, 117);
+            this.buttonWoman.Name = "buttonWoman";
+            this.buttonWoman.Size = new System.Drawing.Size(158, 73);
+            this.buttonWoman.TabIndex = 7;
+            this.buttonWoman.Text = "woman";
+            this.buttonWoman.UseVisualStyleBackColor = true;
+            this.buttonWoman.Click += new System.EventHandler(this.buttonWoman_Click);
+            // 
+            // buttonDog
+            // 
+            this.buttonDog.Location = new System.Drawing.Point(494, 117);
+            this.buttonDog.Name = "buttonDog";
+            this.buttonDog.Size = new System.Drawing.Size(134, 73);
+            this.buttonDog.TabIndex = 8;
+            this.buttonDog.Text = "dog";
+            this.buttonDog.UseVisualStyleBackColor = true;
+            this.buttonDog.Click += new System.EventHandler(this.buttonDog_Click);
+            // 
+            // buttonCat
+            // 
+            this.buttonCat.Location = new System.Drawing.Point(645, 117);
+            this.buttonCat.Name = "buttonCat";
+            this.buttonCat.Size = new System.Drawing.Size(117, 73);
+            this.buttonCat.TabIndex = 9;
+            this.buttonCat.Text = "cat";
+            this.buttonCat.UseVisualStyleBackColor = true;
+            this.buttonCat.Click += new System.EventHandler(this.buttonCat_Click);
+            // 
+            // buttonCar
+            // 
+            this.buttonCar.Location = new System.Drawing.Point(779, 117);
+            this.buttonCar.Name = "buttonCar";
+            this.buttonCar.Size = new System.Drawing.Size(129, 73);
+            this.buttonCar.TabIndex = 10;
+            this.buttonCar.Text = "car";
+            this.buttonCar.UseVisualStyleBackColor = true;
+            this.buttonCar.Click += new System.EventHandler(this.buttonCar_Click);
+            // 
+            // buttonBicyle
+            // 
+            this.buttonBicyle.Location = new System.Drawing.Point(930, 117);
+            this.buttonBicyle.Name = "buttonBicyle";
+            this.buttonBicyle.Size = new System.Drawing.Size(163, 73);
+            this.buttonBicyle.TabIndex = 11;
+            this.buttonBicyle.Text = "bicyle";
+            this.buttonBicyle.UseVisualStyleBackColor = true;
+            this.buttonBicyle.Click += new System.EventHandler(this.buttonBicyle_Click);
+            // 
+            // buttonBeautiful
+            // 
+            this.buttonBeautiful.Location = new System.Drawing.Point(228, 205);
+            this.buttonBeautiful.Name = "buttonBeautiful";
+            this.buttonBeautiful.Size = new System.Drawing.Size(163, 63);
+            this.buttonBeautiful.TabIndex = 12;
+            this.buttonBeautiful.Text = "beautiful";
+            this.buttonBeautiful.UseVisualStyleBackColor = true;
+            this.buttonBeautiful.Click += new System.EventHandler(this.buttonBeautiful_Click);
+            // 
+            // buttonBig
+            // 
+            this.buttonBig.Location = new System.Drawing.Point(413, 205);
+            this.buttonBig.Name = "buttonBig";
+            this.buttonBig.Size = new System.Drawing.Size(171, 63);
+            this.buttonBig.TabIndex = 13;
+            this.buttonBig.Text = "big";
+            this.buttonBig.UseVisualStyleBackColor = true;
+            this.buttonBig.Click += new System.EventHandler(this.buttonBig_Click);
+            // 
+            // buttonSmall
+            // 
+            this.buttonSmall.Location = new System.Drawing.Point(605, 205);
+            this.buttonSmall.Name = "buttonSmall";
+            this.buttonSmall.Size = new System.Drawing.Size(151, 63);
+            this.buttonSmall.TabIndex = 14;
+            this.buttonSmall.Text = "small";
+            this.buttonSmall.UseVisualStyleBackColor = true;
+            this.buttonSmall.Click += new System.EventHandler(this.buttonSmall_Click);
+            // 
+            // buttonStrange
+            // 
+            this.buttonStrange.Location = new System.Drawing.Point(779, 205);
+            this.buttonStrange.Name = "buttonStrange";
+            this.buttonStrange.Size = new System.Drawing.Size(151, 63);
+            this.buttonStrange.TabIndex = 15;
+            this.buttonStrange.Text = "strange";
+            this.buttonStrange.UseVisualStyleBackColor = true;
+            this.buttonStrange.Click += new System.EventHandler(this.buttonStrange_Click);
+            // 
+            // buttonLookedAt
+            // 
+            this.buttonLookedAt.Location = new System.Drawing.Point(200, 283);
+            this.buttonLookedAt.Name = "buttonLookedAt";
+            this.buttonLookedAt.Size = new System.Drawing.Size(137, 69);
+            this.buttonLookedAt.TabIndex = 16;
+            this.buttonLookedAt.Text = "looked at";
+            this.buttonLookedAt.UseVisualStyleBackColor = true;
+            this.buttonLookedAt.Click += new System.EventHandler(this.buttonLookedAt_Click);
+            // 
+            // buttonRode
+            // 
+            this.buttonRode.Location = new System.Drawing.Point(357, 283);
+            this.buttonRode.Name = "buttonRode";
+            this.buttonRode.Size = new System.Drawing.Size(121, 69);
+            this.buttonRode.TabIndex = 17;
+            this.buttonRode.Text = "rode";
+            this.buttonRode.UseVisualStyleBackColor = true;
+            this.buttonRode.Click += new System.EventHandler(this.buttonRode_Click);
+            // 
+            // buttonSpokeTo
+            // 
+            this.buttonSpokeTo.Location = new System.Drawing.Point(494, 283);
+            this.buttonSpokeTo.Name = "buttonSpokeTo";
+            this.buttonSpokeTo.Size = new System.Drawing.Size(155, 69);
+            this.buttonSpokeTo.TabIndex = 18;
+            this.buttonSpokeTo.Text = "spoke to";
+            this.buttonSpokeTo.UseVisualStyleBackColor = true;
+            this.buttonSpokeTo.Click += new System.EventHandler(this.buttonSpokeTo_Click);
+            // 
+            // buttonLaughedAt
+            // 
+            this.buttonLaughedAt.Location = new System.Drawing.Point(665, 283);
+            this.buttonLaughedAt.Name = "buttonLaughedAt";
+            this.buttonLaughedAt.Size = new System.Drawing.Size(161, 69);
+            this.buttonLaughedAt.TabIndex = 19;
+            this.buttonLaughedAt.Text = "laughed at";
+            this.buttonLaughedAt.UseVisualStyleBackColor = true;
+            this.buttonLaughedAt.Click += new System.EventHandler(this.buttonLaughedAt_Click);
+            // 
+            // buttonDrove
+            // 
+            this.buttonDrove.Location = new System.Drawing.Point(841, 283);
+            this.buttonDrove.Name = "buttonDrove";
+            this.buttonDrove.Size = new System.Drawing.Size(135, 69);
+            this.buttonDrove.TabIndex = 20;
+            this.buttonDrove.Text = "drove";
+            this.buttonDrove.UseVisualStyleBackColor = true;
+            this.buttonDrove.Click += new System.EventHandler(this.buttonDrove_Click);
+            // 
+            // buttonSpace
+            // 
+            this.buttonSpace.Location = new System.Drawing.Point(366, 370);
+            this.buttonSpace.Name = "buttonSpace";
+            this.buttonSpace.Size = new System.Drawing.Size(171, 63);
+            this.buttonSpace.TabIndex = 21;
+            this.buttonSpace.Text = "Space";
+            this.buttonSpace.UseVisualStyleBackColor = true;
+            this.buttonSpace.Click += new System.EventHandler(this.buttonSpace_Click);
+            // 
+            // buttonPeriod
+            // 
+            this.buttonPeriod.Location = new System.Drawing.Point(565, 370);
+            this.buttonPeriod.Name = "buttonPeriod";
+            this.buttonPeriod.Size = new System.Drawing.Size(91, 63);
+            this.buttonPeriod.TabIndex = 22;
+            this.buttonPeriod.Text = ".";
+            this.buttonPeriod.UseVisualStyleBackColor = true;
+            this.buttonPeriod.Click += new System.EventHandler(this.buttonPeriod_Click);
+            // 
+            // buttonExclamation
+            // 
+            this.buttonExclamation.Location = new System.Drawing.Point(685, 370);
+            this.buttonExclamation.Name = "buttonExclamation";
+            this.buttonExclamation.Size = new System.Drawing.Size(89, 63);
+            this.buttonExclamation.TabIndex = 23;
+            this.buttonExclamation.Text = "!";
+            this.buttonExclamation.UseVisualStyleBackColor = true;
+            this.buttonExclamation.Click += new System.EventHandler(this.buttonExclamation_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(366, 559);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(189, 61);
+            this.buttonClear.TabIndex = 24;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(593, 559);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(197, 61);
+            this.buttonExit.TabIndex = 25;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // sentenceDisplay
+            // 
+            this.sentenceDisplay.Location = new System.Drawing.Point(10, 454);
+            this.sentenceDisplay.Multiline = true;
+            this.sentenceDisplay.Name = "sentenceDisplay";
+            this.sentenceDisplay.Size = new System.Drawing.Size(1230, 59);
+            this.sentenceDisplay.TabIndex = 26;
+            this.sentenceDisplay.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // SentenceBuilder
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1252, 656);
+            this.Controls.Add(this.sentenceDisplay);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonExclamation);
+            this.Controls.Add(this.buttonPeriod);
+            this.Controls.Add(this.buttonSpace);
+            this.Controls.Add(this.buttonDrove);
+            this.Controls.Add(this.buttonLaughedAt);
+            this.Controls.Add(this.buttonSpokeTo);
+            this.Controls.Add(this.buttonRode);
+            this.Controls.Add(this.buttonLookedAt);
+            this.Controls.Add(this.buttonStrange);
+            this.Controls.Add(this.buttonSmall);
+            this.Controls.Add(this.buttonBig);
+            this.Controls.Add(this.buttonBeautiful);
+            this.Controls.Add(this.buttonBicyle);
+            this.Controls.Add(this.buttonCar);
+            this.Controls.Add(this.buttonCat);
+            this.Controls.Add(this.buttonDog);
+            this.Controls.Add(this.buttonWoman);
+            this.Controls.Add(this.buttonMan);
+            this.Controls.Add(this.lowercaseThe);
+            this.Controls.Add(this.uppercaseThe);
+            this.Controls.Add(this.lowercaseAn);
+            this.Controls.Add(this.buttonAn);
+            this.Controls.Add(this.lowercaseA);
+            this.Controls.Add(this.upcaseA);
+            this.Name = "SentenceBuilder";
+            this.Text = "Sentence Builder";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button upcaseA;
+        private System.Windows.Forms.Button lowercaseA;
+        private System.Windows.Forms.Button buttonAn;
+        private System.Windows.Forms.Button lowercaseAn;
+        private System.Windows.Forms.Button uppercaseThe;
+        private System.Windows.Forms.Button lowercaseThe;
+        private System.Windows.Forms.Button buttonMan;
+        private System.Windows.Forms.Button buttonWoman;
+        private System.Windows.Forms.Button buttonDog;
+        private System.Windows.Forms.Button buttonCat;
+        private System.Windows.Forms.Button buttonCar;
+        private System.Windows.Forms.Button buttonBicyle;
+        private System.Windows.Forms.Button buttonBeautiful;
+        private System.Windows.Forms.Button buttonBig;
+        private System.Windows.Forms.Button buttonSmall;
+        private System.Windows.Forms.Button buttonStrange;
+        private System.Windows.Forms.Button buttonLookedAt;
+        private System.Windows.Forms.Button buttonRode;
+        private System.Windows.Forms.Button buttonSpokeTo;
+        private System.Windows.Forms.Button buttonLaughedAt;
+        private System.Windows.Forms.Button buttonDrove;
+        private System.Windows.Forms.Button buttonSpace;
+        private System.Windows.Forms.Button buttonPeriod;
+        private System.Windows.Forms.Button buttonExclamation;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.TextBox sentenceDisplay;
+    }
+}
+
---
+++ SentenceBuilder/Form1.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SentenceBuilder
+{
+    public partial class SentenceBuilder : Form
+    {
+        public SentenceBuilder()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonSpokeTo_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonSpokeTo.Text;
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Clear();
+        }
+
+        private void buttonDrove_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonDrove.Text;
+            
+        }
+
+        private void upcaseA_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += upcaseA.Text;
+        }
+
+        private void lowercaseA_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += lowercaseA.Text;
+        }
+
+        private void buttonAn_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonAn.Text;
+        }
+
+        private void lowercaseAn_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += lowercaseAn.Text;
+        }
+
+        private void uppercaseThe_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += uppercaseThe.Text;
+        }
+
+        private void lowercaseThe_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += lowercaseThe.Text;
+        }
+
+        private void buttonMan_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonMan.Text;
+        }
+
+        private void buttonWoman_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonWoman.Text;
+        }
+
+        private void buttonDog_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonDog.Text;
+        }
+
+        private void buttonCat_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonCat.Text;
+        }
+
+        private void buttonCar_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonCar.Text;
+        }
+
+        private void buttonBicyle_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonBicyle.Text;
+        }
+
+        private void buttonBeautiful_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonBeautiful.Text;
+        }
+
+        private void buttonBig_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonBig.Text;
+        }
+
+        private void buttonSmall_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonSmall.Text;
+        }
+
+        private void buttonStrange_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonStrange.Text;
+        }
+
+        private void buttonLookedAt_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonLookedAt.Text;
+        }
+
+        private void buttonRode_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonRode.Text;
+        }
+
+        private void buttonLaughedAt_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonLookedAt.Text;
+        }
+
+        private void buttonSpace_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += " ";
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void buttonExclamation_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonExclamation.Text;
+        }
+
+        private void buttonPeriod_Click(object sender, EventArgs e)
+        {
+            sentenceDisplay.Text += buttonPeriod.Text;
+
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        
+
+        }
+    }
+}
